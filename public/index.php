110cd27f03c035d213bd392e49aa52c03854cc0c
abimaelst <abimaest@gmail.com>
2022-03-14 03:37:28 -0300

    creating method get all

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(['name' => 'abimael', 'email' => 'abimael@gail.com']);
+// $user = new User(['name' => 'abimael', 'email' => 'abimael@gail.com']);
 
-print_r($user);
+print_r(User::get(['name' => 'Admin'], 'name, id'));
+
+foreach (User::get([], 'name') as $user) {
+    echo $user->name;
+    echo '<br>';
+}
